a0ce712e5ccfe4c29023bc8541ae1faa9132fc5e
Luka Lazarević <lukalazarevic00@gmail.com>
2024-04-07 16:19:04 +0200

    njk, tema

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,6 +4,10 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.filetype.add({
+	extension = { njk = "njk" },
+})
+
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = false
 
@@ -573,12 +577,13 @@ require("lazy").setup({
 					-- `friendly-snippets` contains a variety of premade snippets.
 					--    See the README about individual language/framework/plugin snippets:
 					--    https://github.com/rafamadriz/friendly-snippets
-					-- {
-					--   'rafamadriz/friendly-snippets',
-					--   config = function()
-					--     require('luasnip.loaders.from_vscode').lazy_load()
-					--   end,
-					-- },
+					{
+						"rafamadriz/friendly-snippets",
+						config = function()
+							require("luasnip").filetype_extend("njk", { "html" })
+							require("luasnip.loaders.from_vscode").lazy_load()
+						end,
+					},
 				},
 			},
 			"saadparwaiz1/cmp_luasnip",

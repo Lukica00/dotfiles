d3d3b26ea4aa91a7a9cd4a8a5ee9ea9150aab3b8
Luka Lazarević <lukalazarevic00@gmail.com>
2024-04-16 15:01:59 +0200

    zadovoljan s odabirom tastature

--- dwm/.local/src/dmenu/config.h
+++ dwm/.local/src/dmenu/config.h
@@ -4,14 +4,20 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=11"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const char dark[]    = "#1a1b26";
+static const char light[]   = "#a9b1d6";
+static const char accent[]  = "#c0caf5";
+static const char accent_light[] = "#bb9af7";
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { light, dark },
+	[SchemeSel] = { dark, accent },
+	[SchemeOut] = { dark, accent_light },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
--- dwm/.local/src/dwm/config.h
+++ dwm/.local/src/dwm/config.h
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", dark, "-nf", light, "-sb", accent, "-sf", dark, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {

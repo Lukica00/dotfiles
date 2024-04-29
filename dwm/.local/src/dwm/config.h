46d200053dcd84f04acb9099d00aeff049b9445e
Luka Lazarević <lukalazarevic00@gmail.com>
2024-04-29 23:06:10 +0200

    Prozori za diskord i stim

--- dwm/.local/src/dwm/config.h
+++ dwm/.local/src/dwm/config.h
@@ -34,8 +34,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "discord",  NULL,       NULL,           1<<1,         0,           1  },
+	{ "steam",    NULL,       NULL,           1<<6,         0,           1  },
+	{ "steam",    NULL,       "Friends List", 1<<6,         1,           1  },
 };
 
 /* layout(s) */
